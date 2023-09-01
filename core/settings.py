c2f89bbc70ff769d6894c2b8714da7f171e381be
SamuelBarbosaDev <samueloficial@protonmail.com>
2023-09-01 10:17:27 -0300

    feat: Adicionando as configurações do DRF

--- core/settings.py
+++ core/settings.py
@@ -41,6 +41,16 @@ INSTALLED_APPS = [
     'model',
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_PARSER_CLASSES': [
+        'rest_framework.parsers.JSONParser',
+        'rest_framework.parsers.MultiPartParser',  # Adicione esta linha
+    ],
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+    ],
+}
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
